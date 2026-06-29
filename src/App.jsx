ca2b3336ff33241f2f5a262b957bf8deb528d1a6
Travis Kidd <Caddyshackkidd@gmail.com>
2026-06-29 09:31:37 -0600

    cursor drag speed up

--- src/App.jsx
+++ src/App.jsx
@@ -141,8 +141,8 @@ function CustomCursor() {
     window.addEventListener("mousemove", onMove);
     let raf;
     const tick = () => {
-      cur.current.x += (pos.current.x - cur.current.x) * 0.25;
-      cur.current.y += (pos.current.y - cur.current.y) * 0.25;
+      cur.current.x += (pos.current.x - cur.current.x) * 0.95;
+      cur.current.y += (pos.current.y - cur.current.y) * 0.95;
       if (ringRef.current) ringRef.current.style.transform = `translate(${cur.current.x-16}px,${cur.current.y-16}px)`;
       if (dotRef.current)  dotRef.current.style.transform  = `translate(${pos.current.x-3}px,${pos.current.y-3}px)`;
       raf = requestAnimationFrame(tick);
